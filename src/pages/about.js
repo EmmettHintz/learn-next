1f87ebf83cc744b8300cfe3909f3a72cb6100ceb
EmmettHintz <eahintz@colgate.edu>
2023-04-30 15:31:39 -0400

    Added images and descriptions for Emmett and Mark

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const About = () => {
   return (
@@ -7,7 +8,7 @@ const About = () => {
         <h1 className="text-3xl font-bold text-dark mb-8 text-center">
           About Us
         </h1>
-        <div className="bg-light shadow-lg rounded p-6">
+        <div className="bg-light shadow-lg rounded p-6 hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-rose-50">
           <p className="text-dark text-lg">
             Welcome! We are a dedicated team of students driven by our passion
             for creating groundbreaking solutions tailored to our customers'
@@ -22,6 +23,44 @@ const About = () => {
         <h2 className="text-center text-dark text-3xl font-bold pt-16 ">
           Meet Our Team
         </h2>
+        <div className="grid grid-cols-2 gap-x-6 pt-8 items-center">
+          <div className="flex justify-center">
+            <Image
+              src="/images/Emmett.jpg"
+              alt="Emmett Hintz"
+              width={250}
+              height={250}
+              className="rounded-3xl shadow-lg hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+            />
+          </div>
+          <div className="bg-light shadow-lg rounded p-4 hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-rose-50">
+            <h3 className="text-dark text-4xl font-bold">Emmett Hintz</h3>
+            <p className="text-dark text-2xl underline">Front-End Specialist</p>
+            <p className="text-dark text-xl ">
+              Emmett is a sophomore at the Colgate University studying Computer
+              Science and Economics. He is passionate about web design and
+              creaing interactive and fun websites!
+            </p>
+          </div>
+          <div className="bg-light shadow-lg rounded p-4 hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-rose-50">
+            <h3 className="text-dark text-4xl font-bold">Mark Attar</h3>
+            <p className="text-dark text-2xl underline">Product Manager</p>
+            <p className="text-dark text-xl ">
+              Mark is a sophomore at the Colgate University studying Computer
+              Science. Filler here!
+            </p>
+          </div>
+          <div className="flex justify-center pt-16">
+            <Image
+              src="/images/Mark.jpeg"
+              alt="Mark Attar"
+              width={250}
+              height={250}
+              className="rounded-3xl shadow-lg hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+            />
+          </div>
+          
+        </div>
       </div>
     </div>
   );
