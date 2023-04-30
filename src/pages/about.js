124d1021739b5bf89ad6f41b479f6d4f627a0921
EmmettHintz <eahintz@colgate.edu>
2023-04-30 15:36:08 -0400

    Attempting to deploy now

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,13 +11,14 @@ const About = () => {
         <div className="bg-light shadow-lg rounded p-6 hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-rose-50">
           <p className="text-dark text-lg">
             Welcome! We are a dedicated team of students driven by our passion
-            for creating groundbreaking solutions tailored to our customers'
-            unique needs. Our mission is to exceed expectations by delivering
-            the finest customized products while upholding the highest standards
-            of professionalism. Continuously evolving and growing, we are guided
-            by our core values of integrity, innovation, and excellence. We are
-            grateful for your interest in our company and eagerly anticipate the
-            opportunity to collaborate and create a lasting partnership.
+            for creating groundbreaking solutions tailored to our
+            customers&apos; unique needs. Our mission is to exceed expectations
+            by delivering the finest customized products while upholding the
+            highest standards of professionalism. Continuously evolving and
+            growing, we are guided by our core values of integrity, innovation,
+            and excellence. We are grateful for your interest in our company and
+            eagerly anticipate the opportunity to collaborate and create a
+            lasting partnership.
           </p>
         </div>
         <h2 className="text-center text-dark text-3xl font-bold pt-16 ">
@@ -39,7 +40,7 @@ const About = () => {
             <p className="text-dark text-xl ">
               Emmett is a sophomore at the Colgate University studying Computer
               Science and Economics. He is passionate about web design and
-              creaing interactive and fun websites!
+              creating interactive and fun websites!
             </p>
           </div>
           <div className="bg-light shadow-lg rounded p-4 hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-rose-50">
@@ -59,7 +60,6 @@ const About = () => {
               className="rounded-3xl shadow-lg hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
             />
           </div>
-          
         </div>
       </div>
     </div>
